d4ca2d5852969a2a642ee8466c725d344a609df4
justinpolchinski <justinpolchinski@gmail.com>
2018-04-29 21:08:49 -0400

    changed sleeping in sql

--- baby_db.sql
+++ baby_db.sql
@@ -8,5 +8,5 @@ CREATE TABLE babyThings (
   id INT(5) AUTO_INCREMENT PRIMARY KEY,
   diaperChange VARCHAR(255) NOT NULL,
   feeding VARCHAR(255) NOT NULL,
-  sleeping Decimal(10,3) NOT NULL
+  sleeping VARCHAR(255) NOT NULL
 );
